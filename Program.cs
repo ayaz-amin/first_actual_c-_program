f2b397abf93e172780f3ef51559f11529c6cdb02
Ahmad Ayaz Amin <ayaza2004@outlook.com>
2020-08-29 15:12:22 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharp_stuff
+{
+    class MathStuff
+    {
+        static void Pattern(int[] args)
+        {
+            string message = "The pattern goes like this:";
+            
+            Console.WriteLine(message);
+            for (int i = args[0]; i < args[1]; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Select starting number");
+            string starting_num_str = Console.ReadLine();
+            int starting_num_int = int.Parse(starting_num_str);
+
+            Console.WriteLine("Select ending number");
+            string ending_number_str = Console.ReadLine();
+            int ending_number_int = int.Parse(ending_number_str);
+
+            int[] args_list = {starting_num_int, ending_number_int};
+            MathStuff.Pattern(args_list);
+        }
+    }
+}
